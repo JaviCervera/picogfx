0d75d29d784e4de11422e07e990f1221aaa9cc5a
JaviCervera <JaviCervera@users.noreply.github.com>
2021-04-05 14:02:20 +0200

    Fixes for Emscripten

--- examples/triangle.cpp
+++ examples/triangle.cpp
@@ -1,3 +1,6 @@
+#ifdef EMSCRIPTEN
+#include <emscripten.h>
+#endif
 #include <stdio.h>
 #include <string.h>
 #include "../include/picogfx.h"
@@ -5,6 +8,19 @@
 #include "util.h"
 
 using namespace picogfx;
+
+void Update();
+
+GLFWwindow* window = NULL;
+float projection[16];
+float modelView[16];
+float view[16];
+float model[16];
+RenderData* renderData = NULL;
+Geom* geom = NULL;
+double lastTime = 0.0;
+float deltaTime = 0.0f;
+float angle = 0.0f;
 
 int main(int argc, char* argv[]) {
     // Init GLFW
@@ -16,7 +32,7 @@ int main(int argc, char* argv[]) {
     // Create window
     glfwWindowHint(GLFW_SAMPLES, 8);
     glfwWindowHint(GLFW_RESIZABLE, true);
-    GLFWwindow* window = glfwCreateWindow(800, 600, "", NULL, NULL);
+    window = glfwCreateWindow(800, 600, "", NULL, NULL);
     if (!window) {
         puts("Could not open window.\n");
         glfwTerminate();
@@ -32,12 +48,6 @@ int main(int argc, char* argv[]) {
         return -1;
     }
 
-    // Define matrices
-    float projection[16];
-    float modelView[16];
-    float view[16];
-    float model[16];
-
     // Create shader
     char shaderError[256];
     char* vertex = LoadString("data/vertexcolor.vs.glsl");
@@ -52,7 +62,7 @@ int main(int argc, char* argv[]) {
     }
 
     // Create render data
-    RenderData* renderData = RenderData::Create(*shader);
+    renderData = RenderData::Create(*shader);
     renderData->BindMat4("Projection", projection);
     renderData->BindMat4("ModelView", modelView);
 
@@ -63,40 +73,44 @@ int main(int argc, char* argv[]) {
         Vertex(1, -1, 0, 0xff0000ff)
     };
     const unsigned short indices[] = {0, 1, 2};
-    Geom* geom = Geom::Create(vertices, 3, indices, 3);
+    geom = Geom::Create(vertices, 3, indices, 3);
 
     // Main loop
-    double lastTime = glfwGetTime();
-    float deltaTime = 0.0f;
-    float angle = 0.0f;
+    lastTime = glfwGetTime();
+#ifdef __EMSCRIPTEN__
+    emscripten_set_main_loop(Update, 0, true);
+#else
     while (!glfwWindowShouldClose(window) && !glfwGetKey(window, GLFW_KEY_ESCAPE)) {
-        // Update delta time
-        deltaTime = float(glfwGetTime() - lastTime);
-        lastTime = glfwGetTime();
-
-        // Update angle
-        angle += 90 * deltaTime;
-
-        // Prepare for drawing
-        int width, height;
-        glfwGetWindowSize(window, &width, &height);
-        Core::Get().SetPerspective(60, float(width) / height, 1, 1000, projection);
-        Core::Get().SetView(0, 0, -4, 0, 0, 0, view);
-        Core::Get().SetTransform(0, 0, 0, 0, angle, 0, 1, 1, 1, model);
-        Core::Get().MulMatrices(view, model, modelView);
-        Core::Get().Prepare(0, 0, width, height, 0xff000044);
-
-        // Render geom
-        geom->Render(*renderData, false);
-
-        // Swap screen buffers
-        glfwSwapBuffers(window);
-
-        // Poll events
-        glfwPollEvents();
+        Update();
     }
-
-    // Shutdown
     glfwTerminate();
+#endif
     return 0;
+}
+
+void Update() {
+    // Update delta time
+    deltaTime = float(glfwGetTime() - lastTime);
+    lastTime = glfwGetTime();
+
+    // Update angle
+    angle += 90 * deltaTime;
+
+    // Prepare for drawing
+    int width, height;
+    glfwGetWindowSize(window, &width, &height);
+    Core::Get().SetPerspective(60, float(width) / height, 1, 1000, projection);
+    Core::Get().SetView(0, 0, -4, 0, 0, 0, view);
+    Core::Get().SetTransform(0, 0, 0, 0, angle, 0, 1, 1, 1, model);
+    Core::Get().MulMatrices(view, model, modelView);
+    Core::Get().Prepare(0, 0, width, height, 0xff000044);
+
+    // Render geom
+    geom->Render(*renderData, false);
+
+    // Swap screen buffers
+    glfwSwapBuffers(window);
+
+    // Poll events
+    glfwPollEvents();
 }
