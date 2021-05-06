e4f4bcff1ddebbf8d9abb0695fa6817369085691
JaviCervera <JaviCervera@users.noreply.github.com>
2021-05-06 10:11:40 +0200

    Added Core::RotateVec

--- src/core.cpp
+++ src/core.cpp
@@ -48,6 +48,11 @@ void Core::MulVecByMatrix(const float* vec, const float* m, float* out) {
     memcpy(out, &result, sizeof(result));
 }
 
+void Core::RotateVec(float x, float y, float z, float pitch, float yaw, float roll, float* out) {
+    lvec3_t result = lquat_mulvec3(lquat_fromeuler(lvec3_rad(lvec3(pitch, yaw, roll))), lvec3(x, y, z));
+    memcpy(out, &result, sizeof(result));
+}
+
 void Core::Prepare(int viewportX, int viewportY, int viewportWidth, int viewportHeight, int color) {
     GfxDriver::Get().Prepare(viewportX, viewportY, viewportWidth, viewportHeight, color);
 }
